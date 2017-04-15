a7d850cc254657d58ae0448bbd1c824fccfdcb63
y2g.de <yannick.habecker@gmail.com>
2017-04-15 21:58:48 +0200

    cleaning code

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,4 @@
 
-//#define DEBUG
-//if TEST, then the dll starts in test mode and runs all test in a python/test/gamemode.py file
-//#define TEST
-
 #include <stdio.h>
 #include <string.h>
 #include "sampgdk.h"
