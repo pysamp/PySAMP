17f9c855facb4e6a3c5e183be6b16a9370631eef
yannick.cc <yannick.habecker@googlemail.com>
2017-04-21 21:43:06 +0200

    added threading callbacks

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,10 @@
 #include "pysamp/pysamp.h"
 #include "bindings/callbacks.h"
 #include "test/callbackstest.h"
+#include <thread>
 
+std::thread threading;
+bool threadingInitialized = false;
 
 PLUGIN_EXPORT unsigned int PLUGIN_CALL Supports()
 {
@@ -21,7 +24,13 @@ PLUGIN_EXPORT bool PLUGIN_CALL Load(void **ppData)
 		return false;
 	}
 	
-	return sampgdk::Load(ppData);
+	bool sgdk = sampgdk::Load(ppData);
+	if (sgdk) {
+		PySAMP::callback("OnThreadingInit", NULL);
+		threading = std::thread([] { PySAMP::callback("OnThreadingInit", NULL); });
+
+	}
+	return false;
 }
 
 PLUGIN_EXPORT void PLUGIN_CALL Unload()
@@ -31,6 +40,8 @@ PLUGIN_EXPORT void PLUGIN_CALL Unload()
 
 PLUGIN_EXPORT void PLUGIN_CALL ProcessTick()
 {
+	PySAMP::callback("OnThreadingStopSignal", NULL);
+	threading.join();
 	sampgdk::ProcessTick();
 	PySAMP::callback("OnProcessTick", NULL);
 }
