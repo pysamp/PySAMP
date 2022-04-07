75a2f8ac162c58b5607f24f38fe57a135317aca0
denNorske <den@ducky.rocks>
2022-04-07 14:28:52 +0200

    Complete menu.py module with docs and typing.

--- pysamp/menu.py
+++ pysamp/menu.py
@@ -7,6 +7,11 @@ from samp import INVALID_MENU
 
 
 class Menu:
+    """Create and manage menus using this class.
+
+    Create a new menu with :meth:`create`. Then add items to it with
+    :meth:`add_item`, and finally show it to players using :meth:`show`.
+    """
     def __init__(
         self,
         id: int
@@ -32,6 +37,13 @@ class Menu:
         :param float column_1_width: The width of the first column.
         :param optional float column_2_width: The width of the second column.
         :return: Returns an instance of :class:`~pysamp.menu.Menu`.
+
+        .. note::  If the title's length is equal to or greater than 32 chars,
+            the title is truncated to 30 characters.
+            You can only create and access 2 columns (0 & 1).
+
+        .. warning:: There is a limit of 12 items per menu, and a limit of
+            128 menus in total.
         """
         return cls(
             create_menu(
@@ -49,7 +61,7 @@ class Menu:
         .. note:: You can only have 12 items per menu.
             You can only add 8 color codes per one item.
         """
-        if is_valid_menu(self.id):
+        if self.is_valid():
             add_menu_item(self.id, column, text)
         return
 
@@ -58,7 +70,7 @@ class Menu:
 
         :return: No return value.
         """
-        if is_valid_menu(self.id):
+        if self.is_valid():
             destroy_menu(self.id)
         return
 
@@ -67,7 +79,7 @@ class Menu:
 
         :return: No return value.
         """
-        if is_valid_menu(self.id):
+        if self.is_valid():
             disable_menu(self.id)
         return
 
@@ -77,13 +89,13 @@ class Menu:
         :param int row: The row to disable. Rows start at 0.
         :return: No return value.
         """
-        if is_valid_menu(self.id):
+        if self.is_valid():
             disable_menu_row(self.id, row)
         return
 
     @staticmethod
     def get_player_menu(player: "Player") -> Optional["Menu"]:
-        """Figure out which menu a has last had open when none is currently
+        """Figure out which menu a has last had open when no menu is currently
         open.
 
         :param Player player: The player to check last open menu for.
@@ -100,12 +112,36 @@ class Menu:
         :param Player player: The player you want to hide the menu for.
         :return: No return value.
         """
-        if is_valid_menu(self.id):
+        if self.is_valid():
             hide_menu_for_player(self.id, player.id)
         return
 
     def is_valid(self) -> bool:
-        """Check if the menu is valid. Returns True or False."""
+        """Check if the menu is valid.
+
+        :return: ``True`` if valid, otherwise ``False``.
+        """
         return is_valid_menu(self.id)
+
+    def set_column_header(self, column: int, text: str) -> None:
+        """Set the menu column header text.
+
+        :param int column: The column you want to set the header for. (0 or 1)
+        :param str text: The header text you would like to set.
+        :return: No return value.
+        """
+        if self.is_valid():
+            set_menu_column_header(self.id, column, text)
+        return
+
+    def show(self, player: "Player") -> None:
+        """Show the menu for a given player.
+
+        :param Player player: The player you would like to see the menu.
+        :return: No return value.
+        """
+        if self.is_valid():
+            show_menu_for_player(self.id, player.id)
+        return
 
 from pysamp.player import Player  # noqa
