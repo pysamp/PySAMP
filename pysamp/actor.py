47ae416acd27cd3adad0fc8a178620d9fced132e
denNorske~ <den@ducky.rocks>
2022-07-06 19:40:01 +0200

    Complete actor docs with documented parameters.

--- pysamp/actor.py
+++ pysamp/actor.py
@@ -36,12 +36,7 @@ class Actor:
 
     @classmethod
     def create(
-        cls,
-        modelid: int,
-        x: float,
-        y: float,
-        z: float,
-        rotation: float
+        cls, modelid: int, x: float, y: float, z: float, rotation: float
     ) -> Optional["Actor"]:
         """Create a new actor.
 
@@ -66,19 +61,35 @@ class Actor:
         return self.id
 
     def destroy(self) -> bool:
-        """Remove a created actor from the world."""
+        """Remove a created actor from the world.
+
+        :returns: No value returned.
+        """
         return destroy_actor(self.id)
 
     def streamed_in(self, for_player: "Player") -> bool:
-        """Check if a player has streamed in the actor."""
+        """Check if a player has streamed in the actor.
+
+        :param Player for_player: Which player you want to check if streamed in
+        :returns: A boolean, ``True`` if the actor is streamed in, otherwise
+            ``False``.
+        """
         return is_actor_streamed_in(self.id, for_player.id)
 
     def get_virtual_world(self) -> int:
-        """Get which virtual world ID the actor is in."""
+        """Get which virtual world ID the actor is in.
+
+        .. note:: If the actor does not exist, this will still return 0.
+
+        :returns: An integer (0-65535) that represents the world ID.
+        """
         return get_actor_virtual_world(self.id)
 
     def virtual_world(self, virtual_world: int) -> bool:
-        """Set the actor to a specific virtual world id."""
+        """Set the actor to a specific virtual world id.
+
+        :returns: No value returned.
+        """
         return set_actor_virtual_world(self.id, virtual_world)
 
     def apply_animation(
@@ -92,7 +103,24 @@ class Actor:
         freeze: bool,
         time: int,
     ) -> bool:
-        """Set an animation on the actor."""
+        """Set an animation on the actor.
+
+        :param str animation_library: The library where the animation resides.
+        :param str animation_name: The specific animation you'd like to apply.
+        :param float delta: The speed to play the animation at. Use ``4.1``.
+        :param bool loop: Set to ``True`` if you want it to loop, otherwise
+            ``False`` if you want it to stop after completion.
+        :param bool lock_x: Choose whether you want to allow the animation to
+            move the Actor along the X-axis, and not return to original
+            position when it is done playing. ``True`` means the Actor will
+            return to original position.
+        :param bool lock_y: Same as ``lock_x`` above, but for the y-axis.
+        :param bool freeze: ``True`` makes the actor freeze when the animation
+            is done playing. ``False`` will not.
+        :param int time: Timer in milliseconds, set to 0 for never-ending
+            loops.
+        :returns: No value returned.
+        """
         return apply_actor_animation(
             self.id,
             animation_library,
@@ -106,15 +134,30 @@ class Actor:
         )
 
     def clear_animations(self) -> bool:
-        """Clear the animations that are in use by the actor, if any."""
+        """Clear the animations that are in use by the actor, if any.
+
+        If you have set an animation to play on the actor, this clears/stops
+        that.
+
+        :returns: No value returned.
+        """
         return clear_actor_animations(self.id)
 
     def get_position(self) -> Tuple[float, float, float]:
-        """Gets the current world coordinates of where the actor is located."""
+        """Gets the current world coordinates of where the actor is located.
+
+        :returns: A tuple with three floats representing the x, y and z
+            world-coordinate.
+        """
         return get_actor_pos(self.id)
 
     def set_position(self, position: Tuple[float, float, float]) -> bool:
-        """Set a new position for the actor."""
+        """Set a new position for the actor.
+
+        :param Tuple position: A tuple with three floats that represents the
+            world coordinate in x, y and z.
+        :returns: No value returned.
+        """
         try:
             x, y, z = position
         except ValueError:
@@ -123,31 +166,57 @@ class Actor:
             return set_actor_pos(self.id, x, y, z)
 
     def get_facing_angle(self) -> float:
-        """Get the actor's facing angle."""
+        """Get the actor's facing angle.
+
+        :returns: Float that represents the current facing angle. (0-360).
+        """
         return get_actor_facing_angle(self.id)
 
     def set_facing_angle(self, angle: float) -> bool:
-        """Set the actor's facing angle."""
+        """Set the actor's facing angle.
+
+        :param float angle: The new facing angle for the actor.
+        :returns: No value returned.
+        """
         return set_actor_facing_angle(self.id, angle)
 
     def get_health(self) -> float:
-        """Get how much health the actor has."""
+        """Get how much health the actor has.
+
+        :returns: A float that represents the current health of the actor.
+        """
         return get_actor_health(self.id)
 
     def set_health(self, health: float) -> bool:
-        """Set the health of the actor."""
+        """Set the health of the actor.
+
+        :param float health: The health to set on the actor (0-100.0).
+        :returns: No value returned.
+        """
         return set_actor_health(self.id, health)
 
     def is_invulnerable(self) -> bool:
-        """See if the actor is invulnerable to damage."""
+        """See if the actor is invulnerable to damage.
+
+        :returns: ``True`` if the actor is invulnerable, otherwise ``False``.
+        """
         return is_actor_invulnerable(self.id)
 
     def set_invulnerable(self, invulnerable: bool = True) -> bool:
-        """Set the actor to be invlunerable to damage."""
+        """Set the actor to be invlunerable to damage.
+
+        :param optional bool invulnerable: Set to ``True`` if you want to set
+            the actor invulnerable to damage (default). Otherwise, set to
+            ``False``.
+        :returns: No value returned.
+        """
         return set_actor_invulnerable(self.id, invulnerable)
 
     def is_valid(self) -> bool:
-        """Check if the actor is valid."""
+        """Check if the actor is valid.
+
+        :returns: Boolean that is ``True`` if valid, otherwise ``False``.
+        """
         return is_valid_actor(self.id)
 
     @event("OnActorStreamIn")
@@ -157,5 +226,6 @@ class Actor:
     @event("OnActorStreamOut")
     def on_stream_out(cls, actorid: int, forplayerid: int):
         return (cls(actorid), Player(forplayerid))
+
 
 from pysamp.player import Player  # noqa
