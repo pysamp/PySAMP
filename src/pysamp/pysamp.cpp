f98d961d7cf91d48790baaaf0e1ec440df01ee8f
The Cheaterman <the.cheaterman@gmail.com>
2022-12-26 21:11:48 +0100

    Allow using pysamp.config() in global module scope (early init)
    Similarly for RegisterCallback and CallRemoteFunction, allow them to be
    used during early initialization.

--- src/pysamp/pysamp.cpp
+++ src/pysamp/pysamp.cpp
@@ -167,15 +167,12 @@ void PySAMP::registerCallback(const std::string& name, const std::string& format
 
 PyObject* PySAMP::pyConfig(PyObject *self, PyObject *args, PyObject *kwargs)
 {
-	if(!PySAMP::isLoaded())
-		return NULL;
-
 	return PySAMP::gamemode->pyConfig(self, args, kwargs);
 }
 
 PyObject* PySAMP::amxParamsToTuple(AMX *amx, const std::string& callback_name, cell *params)
 {
-	if(!PySAMP::isLoaded())
+	if(!PySAMP::isInitialized())
 		return NULL;
 
 	const std::string* format = PySAMP::callbacks->getFormat(callback_name);
@@ -202,7 +199,7 @@ PyObject* PySAMP::amxParamsToTuple(AMX *amx, const std::string& callback_name, c
 
 cell* PySAMP::tupleToAmxParams(PyObject *tuple, bool asReference)
 {
-	if(!PySAMP::isLoaded())
+	if(!PySAMP::isInitialized())
 		return NULL;
 
 	PySAMP::GIL gil;
