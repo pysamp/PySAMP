9adcbb683bf126a3c13a2a1da38f51f839c8fcdc
The Cheaterman <the.cheaterman@gmail.com>
2023-01-05 23:19:44 +0100

    Add warning when users incorrectly call OO handler with callback name

--- pysamp/event.py
+++ pysamp/event.py
@@ -1,4 +1,7 @@
 import functools
+import io
+import traceback
+import warnings
 
 from .callbacks import registry
 
@@ -10,6 +13,17 @@ def event(callback_name):
             @functools.wraps(handler)
             def wrapper(*args):
                 return handler(*args_converter(cls, *args))
+
+            if handler.__name__ == callback_name:
+                message = io.StringIO()
+                traceback.print_stack(file=message, limit=2)
+                location = message.getvalue().rsplit('\n', 4)[0].lstrip()
+                warnings.warn(
+                    f'Handler {handler} has the same name as callback '
+                    f'"{callback_name}", this is probably not what you want.\n'
+                    f'{location}',
+                    stacklevel=2,
+                )
 
             registry.register_callback(
                 callback_name,
