6a4373a0b52431a58cbb18587270a75ff33ff03c
Dan <55022836+Ykpauneu@users.noreply.github.com>
2023-06-30 18:09:54 +0300

    Fixed Player.using_registry (#122)
    Fixed Player.using_registry

--- gamemodes/grandlarc/player.py
+++ gamemodes/grandlarc/player.py
@@ -13,14 +13,31 @@ class Player(BasePlayer):
         self.last_city_selection_tick = 0
 
 
-    def using_registry(func):
+    @classmethod
+    def from_registry_native(cls, player: BasePlayer) -> "Player":
+        if isinstance(player, int):
+            player_id = player
+
+        if isinstance(player, BasePlayer):
+            player_id = player.id
+
+        player = cls._registry.get(player_id)
+
+        if not player:
+            cls._registry[player_id] = player = cls(player_id)
+
+        return player
+
+
+    @classmethod
+    def using_registry(cls, func):
         """
         This should be used to make sure that our callback takes an instance of the class from the dictionary
         """
         @wraps(func)
         def from_registry(*args, **kwargs):
             args = list(args)
-            args[0] = Player.from_registry(args[0])
+            args[0] = cls.from_registry_native(args[0])
             return func(*args, **kwargs)
 
         return from_registry
